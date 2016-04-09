517982b21c0e81318cb5d33d4e44d25293c468b2
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2016-04-08 21:50:33 -0300

    Problem 01 - Sum of natural numbers that are multiples of 3 or 5 below 1000

--- src/com/nilemarbarcelos/Problem01.java
+++ src/com/nilemarbarcelos/Problem01.java
@@ -2,7 +2,17 @@ package com.nilemarbarcelos;
 
 public class Problem01 {
 
+//	If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+//	we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//	Find the sum of all the multiples of 3 or 5 below 1000.
 	public static void main(String[] args) {
+		int sum = 0;
+		for (int number = 0; number < 1000; number++) {
+			if ((number % 3) == 0 || (number % 5) == 0) {
+				sum += number;
+			}
+		}
+		System.out.println("Sum: " + sum);
 		
 	}
 	
